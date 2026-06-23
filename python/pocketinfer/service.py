a26c105d6876fa0294cdd7d9592e3d5b7c347691
Andrew Tergis <andrew@kineticcomp.com>
2026-06-23 13:11:29 -0700

    Fix missing dependencies, bump bhashini models
    Updating bhashini models to include slightly-more-optimized models used
    in the actual Suno Sutra stage demo. This might save a few hundred MB of
    RAM, making ministral-3 more possible to run.
    
    Small change to pocketinfer CLI arguments, fixing a typo.
    
    Running bhashini models as ubuntu user to better align with file system
    permissions and to resolve a virtualenv conflict
    
    Lastly, ensuring that voice model files are pulled by executing
    `pocketinfer-service --update-app`.

--- python/pocketinfer/service.py
+++ python/pocketinfer/service.py
@@ -21,7 +21,7 @@ def main():
     parser.add_argument('--log-level', type=str, default='INFO', help='Logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)')
     parser.add_argument('--app', type=str, default="HearTheWorld", help='Name of the application to run')
     parser.add_argument('--list-apps', action='store_true', help='List available applications and exit')
-    parser.add_argument('--update_app', action='store_true', default=False, help='Install dependencies for the specified application and exit')
+    parser.add_argument('--update-app', action='store_true', default=False, help='Install dependencies for the specified application and exit')
     parser.add_argument('--dummy-board', action='store_true', default=False, help='Do not use hardware features - load audio and image from file')
     parser.add_argument('--audio-file', type=str, help='Path to 16kHz 16-bit wav file to use with dummy board')
     parser.add_argument('--image-file', type=str, help='Path to image file to use with dummy board')
